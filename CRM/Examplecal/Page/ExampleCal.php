428032e5f0084f3b8d451a04a9f2fe4625a03b1e
Tim Otten <totten@civicrm.org>
2023-07-10 14:39:35 -0700

    Generate skeletal page for `civicrm/examplecal`

---
+++ CRM/Examplecal/Page/ExampleCal.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Examplecal_ExtensionUtil as E;
+
+class CRM_Examplecal_Page_ExampleCal extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('ExampleCal'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
